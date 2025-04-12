d0408944832726d1cb826d5f8d5c1b9a47299f54
Wier-dock <sevajakson@gmail.com>
2025-04-12 18:45:38 +0300

    sub_matrix

--- src/code/sub_matrix.c
+++ src/code/sub_matrix.c
@@ -1,1 +1,9 @@
 #include "../headers/matrix.h"
+
+void sub_matrix(int **matrix, int start_row, int start_col, int rows, int cols, int **result) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix[start_row + i][start_col + j];
+        }
+    }
+}

a70e0a71fc365ada3e745ae3816fd1b98033c36d
Wier-dock <sevajakson@gmail.com>
2025-04-13 10:05:55 +0300

    matrix changes

--- src/code/sum_matrix.c
+++ src/code/sum_matrix.c
@@ -1,13 +1,12 @@
 #include "../headers/matrix.h"
 
 int sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result){
-  int sum = 0;
-    for (int i = 0; i < A->rows; i++) {
-        for (int j = 0; j < A->columns; j++) {
-            sum += matrix_t[i][j];  
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            result = matrix_t *A[i][j] + matrix_t *B[i][j];  
         }
     }
     
-    return sum; 
+    return result; 
 }
 
